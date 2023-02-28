e86ce58daf5d4f55e37a2f4f06d8e9db63f81239
Rémi Rahir <rar@odoo.com>
2023-02-28 11:53:36 +0100

    [IMP] helper: introduce colored printing

--- scripts/commands/list_pr.py
+++ scripts/commands/list_pr.py
@@ -1,15 +1,14 @@
 import configparser
-from helpers import get_odoo_prs
+from helpers import get_odoo_prs, print_msg
 
 
 def list_pr(config: configparser.ConfigParser):
     PRs = get_odoo_prs(config)
     if PRs:
-        print("\nPending Odoo Community/Enterprise PRs:")
-        print(
+        print_msg("\nPending Odoo Community/Enterprise PRs:")
+        print_msg(
             "\n".join(
                 [f"\t{version} - {url}" for [version, url] in PRs.items()]
-            )
-        )
+            ))
     else:
-        print("\nThere are no open pull requests...")
+        print_msg("\nThere are no open pull requests...", "WARNING")
