35c0f36e504dae0a6cecddacf7a13ebdba144773
Pierre Rousseau <pro@odoo.com>
2025-11-03 15:13:11 +0100

    [IMP] contributors: add matho

--- scripts/contributors.py
+++ scripts/contributors.py
@@ -1,12 +1,13 @@
 CONTRIBUTORS = [
+    "Florian Damhaut (flda) <flda@odoo.com>",
     "Anthony Hendrickx (anhe) <anhe@odoo.com>",
     "Alexis Lacroix (laa) <laa@odoo.com>",
     "Lucas Lefèvre (lul) <lul@odoo.com>",
-    "Dhrutik Patel (dhrp) <dhrp@odoo.com>",
     "Adrien Minne (adrm) <adrm@odoo.com>",
-    "Ronak Mukeshbhai Bharadiya <rmbh@odoo.com>",
-    "Florian Damhaut (flda) <flda@odoo.com>",
+    "Ronak Mukeshbhai Bharadiya (rmbh) <rmbh@odoo.com>",
+    "Dhrutik Patel (dhrp) <dhrp@odoo.com>",
     "Rémi Rahir (rar) <rar@odoo.com>",
     "Pierre Rousseau (pro) <pro@odoo.com>",
     "Vincent Schippefilt (vsc) <vsc@odoo.com>",
+    "Marceline Thomas (matho) <matho@odoo.com>",
 ]
