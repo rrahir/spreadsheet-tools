33d4f55a105b7f782051bd58641f16ba45c7929e
Pierre Rousseau <pro@odoo.com>
2025-12-03 09:25:58 +0100

    [ADD] sample dashboard script

--- custom-addons/spreadsheet_dashboard_script/static/src/menu_items.js
+++ custom-addons/spreadsheet_dashboard_script/static/src/menu_items.js
@@ -2,6 +2,10 @@
 
 import { registries } from "@odoo/o-spreadsheet";
 import * as scripts from "./scripts/index";
+import {
+  createSpreadsheetModel,
+  freezeOdooData,
+} from "@spreadsheet/helpers/model";
 const { topbarMenuRegistry } = registries;
 
 topbarMenuRegistry.addChild("dashboard-scripts", ["file"], {
@@ -26,3 +30,33 @@ for (const script of Object.values(scripts)) {
 
     })
 }
+
+/**
+ * To use the following menu item, you need to have all spreadsheet_dashboard_*
+ * modules installed and the demo data loaded.
+ *
+ * You can find (at the time of this commit) the command line to load all the
+ * spreadsheet_dashboard_* modules in runbot, in the build "fast testing"
+ */
+topbarMenuRegistry.addChild("create_sample", ["file", "dashboard-scripts"], {
+    name: "Create sample dashboards",
+    async execute(env) {
+        const result = {};
+        const dashboards = await env.services.orm.call(
+            "spreadsheet.dashboard",
+            "get_dashboard_files_for_sample",
+        );
+        for (const [file, data] of Object.entries(dashboards)) {
+            const model = createSpreadsheetModel({ env, data });
+            const dataFrozen = await freezeOdooData(model);
+            dataFrozen.sheets = dataFrozen.sheets.slice(0, -1); // remove last sheet (global filters)
+            dataFrozen.globalFilters = [];
+            result[file] = dataFrozen;
+        }
+        await env.services.orm.call(
+            "spreadsheet.dashboard",
+            "write_dashboard_files",
+            [result]
+        );
+    },
+});
