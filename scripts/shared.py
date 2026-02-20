cfb91324f89d2edcb6a57762421e5e3c2ab7777e
Pierre Rousseau <pro@odoo.com>
2026-02-20 07:42:09 +0100

    [IMP] 19.2 is born

--- scripts/shared.py
+++ scripts/shared.py
@@ -26,6 +26,7 @@ spreadsheet_odoo_versions = {
     "saas-18.4": ["odoo", "saas-18.4", "addons/spreadsheet/static/src/o_spreadsheet/", "o_spreadsheet.esm.js", "SCSS"],
     "19.0": ["odoo", "19.0", "addons/spreadsheet/static/src/o_spreadsheet/", "o_spreadsheet.esm.js", "SCSS"],
     "saas-19.1": ["odoo", "saas-19.1", "addons/spreadsheet/static/src/o_spreadsheet/", "o_spreadsheet.esm.js", "CSS"],
+    "saas-19.2": ["odoo", "saas-19.2", "addons/spreadsheet/static/src/o_spreadsheet/", "o_spreadsheet.esm.js", "CSS"],
     "master": ["odoo", "master", "addons/spreadsheet/static/src/o_spreadsheet/", "o_spreadsheet.esm.js", "CSS"],
 }
 
